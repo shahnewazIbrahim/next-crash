7d8a53f25fafd9e01fee1807152e5d7e958ef783
shahnewazIbrahim <shahnewaz886@gmail.com>
2024-10-16 10:17:05 +0600

    counter meta data

--- app/counter/page.js
+++ app/counter/page.js
@@ -1,23 +1,14 @@
-"use client"
-import { useState } from "react"
+import Counter from "./counter";
 
-export default function Counter() {
-    const [count, setCount] = useState(0)
+export const metadata = {
+    title: "Counter App",
+    description: "Ami ki vebechilam eta ",
+};
 
+export default function CounterPage() {
     return (
-        <>
-        <div className="my-2">
-            {count}
+        <div>
+            <Counter/>
         </div>
-
-        <button className="bg-rose-500 px-2 py-1 rounded-md" onClick={(preveState) => setCount((preveState)=> preveState + 1)}>
-            Add
-        </button>
-
-        <button className="bg-yellow-500 px-2 py-1 rounded-md mx-5" onClick={(preveState) => setCount((preveState)=> 0)}>
-            Reset
-        </button>
-        
-        </>
     )
 }
